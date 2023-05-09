3fa7d836fa731bbe0a4187609759f55437eaba0b
alframirez <alframirezg@gmail.com>
2023-05-09 18:50:50 -0400

    CRUD created for notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 
+// Requerimos dentro de la constante Note el modelo de mongoose Note.js
+const Note = require("../models/Note");
+
 router.get("/notes/add", (req, res) => {
   res.render("notes/new-note");
 });
 
-router.post("/notes/new-note", (req, res) => {
+// Agregar nueva Nota
+router.post("/notes/new-note", async (req, res) => {
   const { title, description } = req.body;
   const errors = [];
   if (!title) {
@@ -20,12 +24,42 @@ router.post("/notes/new-note", (req, res) => {
       description,
     });
   } else {
-    res.send("ok");
+    const newNote = new Note({ title, description });
+    // Guardamos la informacion pasada al modelo en la base de datos
+    await newNote.save();
+    req.flash("success_msg", "Note Added successfully");
+    res.redirect("/notes");
   }
 });
 
-router.get("/notes", (req, res) => {
-  res.send("notes from database");
+// Consultar todas las notas existentes
+
+router.get("/notes", async (req, res) => {
+  // Usamos .find() en el modelo para encontrar todos los documentos dentro de el
+  // .lean para devolver un objeto simple con solo datos
+  // .sort para ordenar por Date el arreglo obtenido
+  const notes = await Note.find().lean().sort({ date: "desc" });
+  res.render("notes/all-notes", { notes });
 });
 
+// Consultar la informacion de una nota existente para editarla
+router.get("/notes/edit/:id", async (req, res) => {
+  const note = await Note.findById(req.params.id).lean();
+  res.render("notes/edit-note", { note });
+});
+
+// Editar una nota existente
+router.put("/notes/edit-note/:id", async (req, res) => {
+  const { title, description } = req.body;
+  await Note.findByIdAndUpdate(req.params.id, { title, description });
+  req.flash("success_msg", "Note updated successfully");
+  res.redirect("/notes");
+});
+
+// Eliminar una nota exisente
+router.delete("/notes/delete/:id", async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id);
+  req.flash("success_msg", "Note deleted successfully");
+  res.redirect("/notes");
+});
 module.exports = router;
